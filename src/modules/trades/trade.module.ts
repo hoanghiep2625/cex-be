9caa953a1ba3085e32a5969ea44dffcb815b888f
hoanghiep2625 <hoanghiep262005@gmail.com>
2025-10-19 00:55:05 +0700

    refactor(trade): remove TradeController from TradeModule

--- src/modules/trades/trade.module.ts
+++ src/modules/trades/trade.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TradeService } from './trade.service';
-import { TradeController } from './trade.controller';
 import { Trade } from './entities/trade.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Trade])],
   providers: [TradeService],
-  controllers: [TradeController],
+  controllers: [],
   exports: [TradeService],
 })
 export class TradeModule {}
